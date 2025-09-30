b0f71ac1e975285cefaaa62b8f91366147e2e45f
joe <josephfielding03@gmail.com>
2025-09-30 19:39:27 -0400

    content script

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,8 +9,12 @@ export default defineConfig({
         outDir: "dist",
         rollupOptions: {
             input: {
-                popup: "index.html"
+                popup: "index.html",
+                content: resolve(__dirname, 'src/content/content.ts')
+            },
+            output: {
+                entryFileNames: '[name].js'
             }
-        }
+        },
     }
 })
