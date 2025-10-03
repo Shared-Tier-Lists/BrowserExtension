b20981779fd92aa1c3e718ce774eb6b10d1da202
joe <josephfielding03@gmail.com>
2025-10-02 23:57:07 -0400

    fetching and opening projects

--- src/ProjectMenuButton.tsx
+++ src/ProjectMenuButton.tsx
@@ -1,21 +1,54 @@
 import {useState} from 'react'
 import ProjectMenu from "./ProjectMenu.tsx";
+import {getUserId, postRequest} from "./util.tsx";
+import type {OpenProjectRequest, OpenProjectResponse, Project, ProjectListRequest} from "./projectRequestTypes.ts";
 
 
-interface ProjectMenuProps {
+interface ProjectMenuButtonProps {
     name: string
 }
 
-const ProjectMenuButton = ({ name }: ProjectMenuProps) => {
+const ProjectMenuButton = ({ name }: ProjectMenuButtonProps) => {
+    const [menuOpen, menuSetOpen] = useState(false);
+    const [projects, setProjects] = useState<Project[]>([]);
 
-    const [popupOpen, setPopupOpen] = useState(false)
+    const projectsListRequest = async () => {
+        const projectList = await postRequest<ProjectListRequest, Project[]>(
+            "http://localhost:3000/open-project-list",
+            {
+                user_id: getUserId(),
+                template_link: window.location.href
+            }
+        );
+
+        setProjects(projectList);
+        menuSetOpen(true);
+    }
+
+    const openProjectRequest = async(projectId: string) => {
+        const project = await postRequest<OpenProjectRequest, OpenProjectResponse>(
+            "http://localhost:3000/open-project",
+            { id: projectId }
+        );
+
+        const tierContainer = document.getElementById("tier-container");
+        if (tierContainer) {
+            tierContainer.innerHTML = project.tier_rows_html;
+        }
+        const imageCarousel = document.getElementById("create-image-carousel");
+        if (imageCarousel) {
+            imageCarousel.innerHTML = project.image_carousel_html;
+        }
+
+        menuSetOpen(false);
+    };
 
     return (
         <>
-            <button className="bg-purple-700 text-white z-[999999]" onClick={() => setPopupOpen(true)}>
+            <button className="bg-purple-600 text-white z-[999999] w-[20vw] h-[10vh] text-[3vh]" onClick={projectsListRequest}>
                 {name}
             </button>
-            <ProjectMenu open={popupOpen} setOpen={setPopupOpen} />
+            <ProjectMenu projects={projects} menuOpen={menuOpen} menuSetOpen={menuSetOpen} openProjectRequest={openProjectRequest} />
         </>
     )
 }
